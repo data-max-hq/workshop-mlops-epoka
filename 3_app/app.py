2a2d14e5119d234ddb2595739925f4588f62c12b
Sadik Bakiu <sbakiu@gmail.com>
2022-04-23 17:48:24 +0200

    Work on model prediction

--- 3_app/app.py
+++ 3_app/app.py
@@ -1,14 +1,30 @@
 import pickle
 import numpy as np
 import logging
+import dill
 
 from flask import Flask, request
+from ml_utils import CleanTextTransformer, SpacyTokenTransformer
 
 logging.basicConfig(level=logging.INFO)
 
 model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
+
+
+def setup_app(app, models_dir="/models"):
+    clean_text_transformer = CleanTextTransformer()
+    spacy_tokenizer = SpacyTokenTransformer()
+
+    with open(f"{models_dir}/tfidf_vectorizer.model", "rb") as model_file:
+        tfidf_vectorizer = dill.load(model_file)
+
+    with open(f"{models_dir}/lr.model", "rb") as model_file:
+        lr_model = dill.load(model_file)
+
+
+setup_app(app)
 
 
 @app.route('/')
@@ -21,6 +37,10 @@ def hello():
 # Refactor when model file is available
 @app.route('/predict', methods=['POST'])
 def predict():
+    # clean_text = self._clean_text_transformer.transform(X)
+    # spacy_tokens = self._spacy_tokenizer.transform(clean_text)
+    # tfidf_features = self._tfidf_vectorizer.transform(spacy_tokens)
+    # predictions = self._lr_model.predict_proba(tfidf_features)
     features = list(request.json.values())
     logging.info(f"Received json: {features}")
 
